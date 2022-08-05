f4cf9394a5a867468302c39b2659ccb9289c1e7f
goodl <goodluckajay@gmail.com>
2022-08-05 12:45:07 +0530

    dsfds

--- src/com/test/CanBeDeleted.java
+++ src/com/test/CanBeDeleted.java
@@ -6,5 +6,6 @@ public class CanBeDeleted {
     public static void main(String[] args) {
         System.out.println("can be deleted");
         System.out.println("added after commit and push");
+        System.out.println("3rd");
     }
 }
